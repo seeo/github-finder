4bfeec931dd44e2292fb5998bd933a5d6a5e2573
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-18 21:20:15 +0800

    made UserItem into a stateless functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class UserItem extends Component {
-
-    render() {
+//beecome stateless functional component
+const UserItem = (props) => {
         //destructure here so that we don't have to keep calling this.state in the return method
-        const {login, avatar_url, html_url} = this.props.user;
+        const {login, avatar_url, html_url} = props.user;
         return (
             <div className = "card text-center">
                 <img
@@ -21,7 +20,6 @@ class UserItem extends Component {
               </div>
             </div>
         )
-    }
 }
 
 export default UserItem
