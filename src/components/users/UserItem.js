7e0a70170eb37d503e41d9f04e463a6b22e54d69
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-18 21:15:28 +0800

    made a container that wrap around Users component in App.js

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-   state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-        };
 
     render() {
-        //destrcuture here so that we don't have to keep calling this.state in the return method
-        const {login, avatar_url, html_url} = this.state;
+        //destructure here so that we don't have to keep calling this.state in the return method
+        const {login, avatar_url, html_url} = this.props.user;
         return (
             <div className = "card text-center">
                 <img
