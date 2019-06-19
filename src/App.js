fde6b91bd2e8366324033169bdec3b650d06e469
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-19 18:51:12 +0800

    managed to create a clear button in App.js

--- src/App.js
+++ src/App.js
@@ -14,10 +14,8 @@ class App extends Component {
 //send state down to components via props
     async componentDidMount() {
         // console.log("client_secret: ", process.env.REACT_APP_GITHUB_CLIENT_SECRET);
-        this.setState({
-            loading: true,
+        this.setState({loading: true,});
 
-        })
         const res = await axios.get(`https://api.github.com/users?client_id=
         $ {
             process.env.REACT_APP_GITHUB_CLIENT_ID
@@ -36,31 +34,40 @@ class App extends Component {
     searchUsers = async text => {
         this.setState({loading: true});
         const res = await axios.get(
-            `https://api.github.com/search/users?q=${text}&client_id=${
-                process.env.REACT_APP_GITHUB_CLIENT_ID
+        `https://api.github.com/search/users?q=${text}&client_id=${
+            process.env.REACT_APP_GITHUB_CLIENT_ID
         }&client_secret=${
-                process.env.REACT_APP_GITHUB_CLIENT_SECRET
-        }`);
+            process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
         this.setState({
             users: res.data.items,
             loading: false,
         });
-    }
+    };
+
+    //clear users from state
+    clearUsers = () => this.setState({
+            users: [],
+            loading: false,
+        });
+
 
     render(){
         return (
         <div className = 'App'>
             <Navbar />
             <div className = "container">
-                <Search searchUsers={this.searchUsers}/>
+                <Search
+                    searchUsers={this.searchUsers}
+                    clearUsers = {this.clearUsers}
+                />
                 <Users
                     loading = {this.state.loading}
                     users = {this.state.users}
-                / >
+                />
             </div>
         </div>
         );
-    }
-}
+    };
+};
 
 export default App;
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class Search extends Component {
+class Search extends Component {
     state = {
         text: '',
     };
 
     static propTypes ={
         searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
     }
     //create onChange arrow function, that takes in an event parameter
     onChange = (e) => {
@@ -41,7 +42,11 @@ export class Search extends Component {
                         className = "btn btn-dark btn-block"
                     />
                 </form>
+                <button
+                    className = "btn btn-light btn-block"
+                    onClick = {this.props.clearUsers}>Clear</button>
             </div>
+
         )
     }
 }
