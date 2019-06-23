92bba6fd565144c52ddcd40c8d490786b5cd39b6
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-23 21:10:11 +0800

    done with listing out user's followers, following, public repos and public gist

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,19 +1,30 @@
 //not going to use a state here, but will use a component life cycle method here
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 export class User extends Component {
     //this fires right away when the User component is loaded
     componentDidMount(){
         this.props.getUser(this.props.match.params.login);
     }
+
+    static propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired,
+    }
+
     render() {
+        //destructuring here...
         const{
             name,
             avatar_url,
             location,
             bio,
             blog,
+            company,
             login,   //login is also the username, as per the Github response.data API
             html_url,
             followers,
@@ -25,10 +36,73 @@ export class User extends Component {
 
         const{loading} = this.props;
 
+        //so that when page is loading, the spinner will appear, as user name is being fetched.
+        if(loading) return (<Spinner />);
+
         return (
-            <div>
-                {name}
-            </div>
+            <Fragment>
+                <Link to = '/' className = 'btn btn-light'>
+                    Back to Search
+                </Link>
+                Hireable: {''}
+                {hireable ? (
+                    <i className = "fas fa-check text-success" />
+                ) : (
+                    <i className = "fas fa-times-circle text-danger" />
+                )}
+                <div className = "card grid-2">
+                    <div className="all-center">
+                        <img
+                            src = {avatar_url}
+                            className = "round-img"
+                            alt ="user's avatar"
+                            style={{width: '150px'}}
+                        />
+                        <h1>{name}</h1>
+                        <p>Location: {location}</p>
+                    </div>
+                    <div>
+                        {bio && (
+                        <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                        )}
+                        <a href = {html_url} className = "btn btn-dark my-1">
+                            Visit Github Profile
+                        </a>
+                        <ul>
+                            <li>
+                                {login &&(
+                                <Fragment>
+                                    <strong>Username: </strong> {login}
+                                </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {company &&(
+                                <Fragment>
+                                    <strong>Company: </strong> {company}
+                                </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {blog &&(
+                                <Fragment>
+                                    <strong>Website: </strong> {blog}
+                                </Fragment>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className = "card text-center">
+                    <div className = "badge badge-primary">Followers: {followers}</div>
+                    <div className = "badge badge-success">Following: {following}</div>
+                    <div className = "badge badge-light">Public Repos: {public_repos}</div>
+                    <div className = "badge badge-dark">Public Gists: {public_gist}</div>
+                </div>
+            </Fragment>
         )
     }
 }
