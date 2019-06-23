d933b3e89625adc9a1820542bc5b37bff12078aa
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-23 21:42:59 +0800

    Repos.JS component done. App.js->User.js->Repos.js->RepoItem.js Able to list out top 5 latest repos

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,19 +1,24 @@
 //not going to use a state here, but will use a component life cycle method here
 import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
 export class User extends Component {
     //this fires right away when the User component is loaded
     componentDidMount(){
+        //simultaneously call getUser and getUserRepos when the componentDidMount runs
         this.props.getUser(this.props.match.params.login);
+        this.props.getUserRepos(this.props.match.params.login);
     }
 
     static propTypes = {
         loading: PropTypes.bool,
         user: PropTypes.object.isRequired,
+        repos: PropTypes.array.isRequired,
         getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
     }
 
     render() {
@@ -32,9 +37,10 @@ export class User extends Component {
             public_repos,
             public_gist,
             hireable,
+
         } = this.props.user;
 
-        const{loading} = this.props;
+        const{loading, repos} = this.props;
 
         //so that when page is loading, the spinner will appear, as user name is being fetched.
         if(loading) return (<Spinner />);
@@ -97,11 +103,13 @@ export class User extends Component {
                     </div>
                 </div>
                 <div className = "card text-center">
-                    <div className = "badge badge-primary">Followers: {followers}</div>
-                    <div className = "badge badge-success">Following: {following}</div>
-                    <div className = "badge badge-light">Public Repos: {public_repos}</div>
-                    <div className = "badge badge-dark">Public Gists: {public_gist}</div>
+                    <div className = "badge badge-primary">Followers: {followers} </div>
+                    <div className = "badge badge-success">Following: {following} </div>
+                    <div className = "badge badge-light">Public Repos: {public_repos} </div>
+                    <div className = "badge badge-dark">Public Gists: {public_gist} </div>
                 </div>
+
+                <Repos repos={repos} />
             </Fragment>
         )
     }
